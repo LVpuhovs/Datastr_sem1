65430f8b18561d2ef677c1b1f02cd3ccb52df09c
Markuss Puhovs <s22puhomark@vea.lv>
2024-02-28 09:47:35 +0200

    New: seminar1

--- src/Datast/Myarraylist.java
+++ src/Datast/Myarraylist.java
@@ -1,5 +1,49 @@
 package Datast;
 
 public class Myarraylist {
-
+	
+	private int[] list;
+	private final int LIST_DEFAULT_SIZE = 10;
+	private int size = LIST_DEFAULT_SIZE;
+	private int counter = 0;
+	
+	//TODO
+	//konstruktori
+	public Myarraylist() {
+		list = new int[size]; 
+	}
+	
+	public Myarraylist(int inputSize) {
+		if (inputSize > 0) {
+			size = inputSize;
+		}
+		list = new int[size];
+	}
+	
+	//Isempty
+	public boolean isEmpty() {
+		//1. long if - else
+		/*
+		if (counter == 0) 
+			return true;
+		else
+			return false;
+		*/
+		//2. short if - else
+		/*
+		return (counter == 0)? true : false;
+		*/
+		//3.
+		return (counter == 0);
+		
+	}
+	
+	//IsFull
+	public boolean isFull() {
+		return (counter == size);
+	}
+	//HowManyEelements
+	public int howManyElements() {
+		return counter;
+	}
 }
